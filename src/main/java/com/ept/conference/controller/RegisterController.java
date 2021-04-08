79131ce62458d943d58f492afc1f156ec590ef93
Elghodhben Oussama <oussamaelghodhben@gmail.com>
2021-04-08 22:19:20 +0100

    finalized v1

--- src/main/java/com/ept/conference/controller/RegisterController.java
+++ src/main/java/com/ept/conference/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.ept.conference.controller;
 
 import com.ept.conference.controller.dto.UserRegistrationDto;
+import com.ept.conference.repositories.UserRepository;
 import com.ept.conference.service.UserServiceImp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +14,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RegisterController {
 
     private UserServiceImp userService;
+    private final UserRepository userRepository;
 
-    public RegisterController(UserServiceImp userService) {
+    public RegisterController(UserServiceImp userService, UserRepository userRepository) {
         super();
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto, Model model) {
-        userService.save(registrationDto);
-        //return "redirect:/login?success";
-        model.addAttribute("success", true);
+        if(userRepository.findByUsername(registrationDto.getUsername()) != null){
+            model.addAttribute("error", "Username already used");
+            model.addAttribute("failure", true);
+        }else if (userRepository.findByEmail(registrationDto.getEmail()) != null){
+            model.addAttribute("error", "Email already used");
+            model.addAttribute("failure", true);
+        }else{
+            userService.save(registrationDto);
+            model.addAttribute("success", true);
+        }
         return "login";
     }
 
